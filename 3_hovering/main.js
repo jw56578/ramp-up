883348037b8c84f181f1e2d259c4b92cbe2f8a45
mikeadossi <mikeadossi@gmail.com>
2016-07-28 02:49:50 -0500

    got onmouseover and onmouseout working

--- 3_hovering/main.js
+++ 3_hovering/main.js
@@ -1,1 +1,27 @@
 // TODO: Experiment with hover effects to make dynamic looking features
+
+var tuQuoqueTab = document.getElementById("tuQuoqueTab");
+var illicitMajorTab = document.getElementById("illicitMajorTab");
+var texasSharpTab = document.getElementById("texasSharpTab");
+
+// we use the onmouseover & onmouseout js functions on our html element, hover requires jQuery
+tuQuoqueTab.onmouseover = function(){
+	tuQuoqueTab.style.background = 'red';
+}
+tuQuoqueTab.onmouseout = function(){
+	tuQuoqueTab.style.background = 'gray';
+}
+
+illicitMajorTab.onmouseover = function(){
+	illicitMajorTab.style.background = 'orange';
+}
+illicitMajorTab.onmouseout = function(){
+	illicitMajorTab.style.background = 'gray';
+}
+
+texasSharpTab.onmouseover = function(){
+	texasSharpTab.style.background = 'skyblue';
+}
+texasSharpTab.onmouseout = function(){
+	texasSharpTab.style.background = 'gray';
+}
