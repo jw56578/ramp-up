1569435ee807dba968fc4ce255409da862cb3182
mikeadossi <mikeadossi@gmail.com>
2016-07-28 04:26:18 -0500

    progress made

--- 3_hovering/main.js
+++ 3_hovering/main.js
@@ -7,23 +7,30 @@ var texasSharpTab = document.getElementById("texasSharpTab");
 // we use the onmouseover & onmouseout js functions on our html element, hover requires jQuery
 tuQuoqueTab.onmouseover = function(){
 	tuQuoqueTab.style.background = 'red';
+	tuQuoqueDropdown.style.display = 'block';
 }
 tuQuoqueTab.onmouseout = function(){
 	tuQuoqueTab.style.background = 'gray';
+	tuQuoqueDropdown.style.display = 'none';
 }
 
 illicitMajorTab.onmouseover = function(){
 	illicitMajorTab.style.background = 'orange';
+	illicitMajorDropdown.style.display = 'inline-block';
 }
 illicitMajorTab.onmouseout = function(){
 	illicitMajorTab.style.background = 'gray';
+	illicitMajorDropdown.style.display = 'none';
 }
 
 texasSharpTab.onmouseover = function(){
 	texasSharpTab.style.background = 'skyblue';
+	texasSharpDropdown.style.display = 'inline-block';
+	texasSharpDropdown.style.display = 'none';
 }
 texasSharpTab.onmouseout = function(){
 	texasSharpTab.style.background = 'gray';
+	texasSharpDropdown.style.display = 'none'
 }
 
-// upon clicking a tab we should see a info bubble dropdown
+// during onmouseover create a dropdown info feature
