d8858d718223aa4fa5f882625775e41e8ae52e21
mikeadossi <mikeadossi@gmail.com>
2016-07-29 16:23:28 -0500

    Got Express to handle our form

--- 4_zellwkMEAN/server.js
+++ 4_zellwkMEAN/server.js
@@ -5,11 +5,12 @@ app.listen(3000, function(){
 	console.log('listening on 3000')
 })
 
-
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html')
 })
 
 // Note: request and response are usually written as req and res.
 
-
+app.post('/quotes', function(req, res){
+	console.log('Hellooooooooooooo!')
+})
