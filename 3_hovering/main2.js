adf44c890c249268d8648e727b44bb83adbff9db
mikeadossi <mikeadossi@gmail.com>
2016-07-28 23:10:13 -0500

    misc changes

--- 3_hovering/main2.js
+++ 3_hovering/main2.js
@@ -1,7 +1,22 @@
-$(tab1).hover( 
+/*$("#tab1").hover( 
 	function(){
 		//$(tab1).css({"background-color":"lightgray"});
-		$(drop1).slideToggle('slow') // or slideDown()
+		$("#drop1").slideToggle('slow') // or slideDown()
   	}
-);
+);*/
 
+$("#tab2").hover( 
+	function(){
+		$("#drop2").css({"visibility":"visible"})
+	},
+	function(){
+		$("#drop2").css({"visibility":"hidden"})
+	}
+);
+/*
+$("#tab3").hover( 
+	function(){
+		//$(tab1).css({"background-color":"lightgray"});
+		$("#drop3").slideToggle('slow') // or slideDown()
+  	}
+); */
