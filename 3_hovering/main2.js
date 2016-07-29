824f533aacc32cf58bd8feeabc4b3e0c4b86ba02
mikeadossi <mikeadossi@gmail.com>
2016-07-28 21:37:51 -0500

    ch-ch-changes

--- 3_hovering/main2.js
+++ 3_hovering/main2.js
@@ -4,3 +4,10 @@ $(tab1).hover(
 		$(drop1).slideToggle('slow') // or slideDown()
   	}
 );
+
+/*$(tab2).hover( 
+	function(){
+		//$(tab1).css({"background-color":"lightgray"});
+		$(drop2).slideToggle('slow') // or slideDown()
+  	}
+);
