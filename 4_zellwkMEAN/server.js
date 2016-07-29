e4018ec563d914fe864e6fa660edaf5d84eca5c8
mikeadossi <mikeadossi@gmail.com>
2016-07-29 15:52:03 -0500

    Simple app built, using the MEAN stack, with help from blog zellwk.com/blog/crud-express-mongodb/

---
+++ 4_zellwkMEAN/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+	console.log('listening on 3000')
+})
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html')
+})
+
+// Note: request and response are usually written as req and res.
+
+
