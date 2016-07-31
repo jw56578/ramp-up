7b69f4a373bfd4519da04da00e323d1716a4ec46
mikeadossi <mikeadossi@gmail.com>
2016-07-31 00:50:02 -0500

    moved MongoDB code to top, removed redundant app.listen code

--- 4_zellwkMEAN/server.js
+++ 4_zellwkMEAN/server.js
@@ -3,11 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
+MongoClient.connect('mongodb://alibaba:0pensesamee@ds015942.mlab.com:15942/mean-beginner', function (err, database) {
+	if (err) return console.log(err)
+	db = database
+	app.listen(3000, function() {
+		console.log('listening on 3000')
+	})
+})
+
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function(){
+/*app.listen(3000, function(){
 	console.log('listening on 3000')
-})
+})*/
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html')
@@ -25,10 +33,3 @@ app.post('/quotes', function(req, res){
 })
 
 
-MongoClient.connect('mongodb://alibaba:0pensesamee@ds015942.mlab.com:15942/mean-beginner', function (err, database) {
-	if (err) return console.log(err)
-	db = database
-	app.listen(3000, function() {
-		console.log('listening on 3000')
-	})
-})
