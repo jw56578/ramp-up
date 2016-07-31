bb5da660e981131c39216d197302f05584b89f4d
mikeadossi <mikeadossi@gmail.com>
2016-07-30 22:37:14 -0500

    edited our post method

--- 4_zellwkMEAN/server.js
+++ 4_zellwkMEAN/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, function(){
 	console.log('listening on 3000')
@@ -12,5 +15,6 @@ app.get('/', function(req, res){
 // Note: request and response are usually written as req and res.
 
 app.post('/quotes', function(req, res){
-	console.log('Hellooooooooooooo!')
+	//console.log('Hellooooooooooooo!') 
+	console.log(req.body)
 })
