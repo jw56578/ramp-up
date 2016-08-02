96fbbca76473b1cbd907bd2263ffb19bf6d92b52
mikeadossi <mikeadossi@gmail.com>
2016-08-02 01:51:09 -0500

    lots done

---
+++ 5_timers/main.js
@@ -0,0 +1,20 @@
+// Goal of this project: write a pokemon game while learning how to use timer functions!!!
+
+// 1st step: figure out how all pokemon will be stored.
+var numbered = 0
+var stringNum = parseInt(numbered)
+var pokemon = 'images/Pkmn'+ stringNum + 'png' // We'll avoid storing the monsters in an array and access them using these 2 variables (pokemon & numbered) instead.
+
+// 2nd step: display starting slide(s)
+
+// 3rd step: create begin game functionality
+
+// 4th step: create timer feature
+
+// 5th step: create timed page refreshes
+
+// 6th step: upload random pictures to screen
+
+// 7th step: have correct pokemon name show up as one of 4 random option each time the image shows
+
+// 8th step: 
