97cb5eebf7cfd0c890d30e3d68850fd89737c2e7
mikeadossi <mikeadossi@gmail.com>
2016-08-06 20:48:25 -0500

    misc changes

--- 5_timers/main.js
+++ 5_timers/main.js
@@ -8,7 +8,7 @@ var pokemon = 'images/Pkmn'+ stringNum + 'png' // We'll avoid storing the monste
 // 2nd step: display starting slide(s), ask player for name to keep track of score.
 
 // Below we create a timer feature!
-time = 50;
+time = 5;
 function timer(){
   if(!time<1){
    time = time - 1
@@ -30,17 +30,20 @@ function randomPokemon(){
 		$('#pokemonImage').attr("src", "Images/Pkmn"+randomNum+".png")
 	}
 	// Below we store a random # between 0 and 4 in a variable (this will help in placing the correct name of the pokemon pictured in a random slot)
-	var randomNumBetween0And4 = Math.floor(Math.random() * 3 + 1);
 	var pokemonNames = 
 ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglypuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hyno","Krabby","Kingler","Voltorb","ELectrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyrados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","FLareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo", "Mew"];
-	var randomize = pokemonNames[randomNum];
-	if(randomNumBetween0And4 = 0){
+	
+	var randomNumBetween0And4 = Math.floor(Math.random() * 3 + 1);
+	var randomNumz = randomNum - 1
+	var randomize = pokemonNames[randomNumz];
+	
+	if(randomNumBetween0And4 = 1){
 		optionA.innerHTML = ""+randomize+"";
-	} else if(randomNumBetween0And4 = 1){
-		optionB.innerHTML = ""+randomize+"";
 	} else if(randomNumBetween0And4 = 2){
-		optionC.innerHTML = ""+randomize+"";
+		optionB.innerHTML = ""+randomize+"";
 	} else if(randomNumBetween0And4 = 3){
+		optionC.innerHTML = ""+randomize+"";
+	} else if(randomNumBetween0And4 = 4){
 		optionD.innerHTML = ""+randomize+"";
 	}
 }
@@ -52,6 +55,7 @@ randomPokemon()
 // TODO: Show checkmark or wrong mark when answer is correct/incorrect in split second.
 
 // TODO: Leaf through each question, maximum of 10 questions.
+// create code that counts the number of pokemon displayed to the user and once that number hits the number you'd like tell the computer to stop the game and go to gameOver screen.
 
 // TODO: calculate the score (add players name to a leaderboard?)
 
